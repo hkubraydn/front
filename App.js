a20793fd69c90a27452e59766263c01732542063
hkubraydn <hatkub.aydin@gmail.com>
2025-03-13 16:44:12 +0300

    Apı Entegre Edildi.

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.subContainer}>
+        <Text style={styles.textStyle}>Bu bir deneme yazısıdır!</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +21,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  subContainer: {
+    backgroundColor: 'blue',
+    width: 70,
+    height: 80,
+    borderRadius: 5,
+  },
+  textStyle: {
+    color: 'white',
+  }
 });
